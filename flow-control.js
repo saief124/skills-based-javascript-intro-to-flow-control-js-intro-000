db62ef305156182a211ed711c523eeb5911c5fa6
Saief I Sayed <saiefsayed123@hotmail.com>
2020-07-21 22:21:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,12 +3,10 @@ if (age>12 && age<20){
   return "You are a teenager!";
 }
 
-}
-
 function teenager(age) {
-if (age>12 && age<20{
+if (age>12 && age<20){
   return "You are a teenager!";
-})
+}
   else {
     return"You are not a teenager";
   }
