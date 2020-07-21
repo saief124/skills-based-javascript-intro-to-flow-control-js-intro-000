cddaa5aed1a6e84234dd0940a0b8c4774d38f401
Saief I Sayed <saiefsayed123@hotmail.com>
2020-07-21 22:19:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,13 +2,16 @@ function basicTeenager(age) {
 if (age>12 && age<20){
   return "You are a teenager!";
 }
-else {
-  return"You are not a teenager";
-}
+
 }
 
 function teenager(age) {
-
+if (age>12 && age<20{
+  return "You are a teenager!";
+})
+  else {
+    return"You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
