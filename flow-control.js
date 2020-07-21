dc0aa96f101ebc8b5b055b2852dd7c7ffef5feb2
Saief I Sayed <saiefsayed123@hotmail.com>
2020-07-21 23:02:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,20 +31,20 @@ return age>12 && age<20 ? "You are a teenager" : "You are not a teenager"
 function switchAge(age) {
 switch(age){
   case "13":
-  return "You are a teenager";
+  return "You are a teenager"
   case "14":
-  return "You are a teenager";
+  return "You are a teenager"
   case "15":
-  return "You are a teenager";
+  return "You are a teenager"
   case "16":
-  return "You are a teenager";
+  return "You are a teenager"
   case "17":
-  return "You are a teenager";
+  return "You are a teenager"
   case "18":
-  return "You are a teenager";
+  return "You are a teenager"
   case "19":
-  return "You are a teenager";
+  return "You are a teenager"
   default:
-  return "You have an age";
+  return "You have an age"
 }
 }
